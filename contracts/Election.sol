3be6ec9e67566e365f0c446cf009a22fe8dbf99c
Hristiyan Nikolov 2 <hnnikolov95@gmail.com2>
2018-10-01 16:45:04 +0300

    initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+/**
+ * The Election contract does this and that...
+ */
+contract Election {
+
+	string public name;
+
+	constructor () public {
+		name = "Hristiyan";
+	}	
+}
